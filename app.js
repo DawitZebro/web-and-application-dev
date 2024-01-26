63eab7159f1d48b2c06247e9d7e539ffccab2db3
DawitZebro <117064382+DawitZebro@users.noreply.github.com>
2024-01-25 17:05:12 -0800

    Add files via upload
    Implement Conditional Routing in an Express NodeJS Server 1

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-ECHO is on.
+
+const express = require('express');
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/foo', (req, res, next) => {
+    
+    if(Math.random() < 0.5) {
+        res.send('sometimes this');
+    } else {
+        next();
+    }
+});
+
+app.get('/foo', (req, res) => {
+    res.send('and sometimes that');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
